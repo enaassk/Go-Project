9e385353b71b0771dcfcc89c435c4a2368d0e8b7
enaassk <114976417+enaassk@users.noreply.github.com>
2024-03-12 13:16:36 +0600

    Add files via upload

---
+++ Go-Project/project/pkg/config/db.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"github.com/enaassk/Go-Project/pkg/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() *gorm.DB {
+	db, err := gorm.Open(postgres.Open("host=localhost dbname=store_db user=postgres password=xzsawq21"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&models.Product{})
+	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.Comment{})
+	db.AutoMigrate(&models.Purchase{})
+	db.AutoMigrate(&models.Rating{})
+	return db
+}
